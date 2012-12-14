18e27a2625dd194434de845ada4f3280bb19df93
luka.zmikic@gmail.com <srnux>
2012-12-14 11:41:20 +0100

    Repository and Controllers modified
    Using Ninject

--- ASPNETMVCNinject/Controllers/PeopleController.cs
+++ ASPNETMVCNinject/Controllers/PeopleController.cs
@@ -7,104 +7,100 @@ using System.Web;
 using System.Web.Mvc;
 using PersonModel;
 using ASPNETMVCNinject.Models;
+using PersonRepository;
 
 namespace ASPNETMVCNinject.Controllers
 {   
     public class PeopleController : Controller
     {
-        private ASPNETMVCNinjectContext context = new ASPNETMVCNinjectContext();
+        private readonly IRepositoryMethods<Person> _peopleRepository; 
 
+        public PeopleController(IRepositoryMethods<Person> peopleRepository)
+        {
+            _peopleRepository = peopleRepository;
+        }
         //
         // GET: /People/
-
         public ViewResult Index()
         {
-            return View(context.People.ToList());
+            return View(_peopleRepository.AllIncluding().ToList());
         }
 
         //
         // GET: /People/Details/5
-
         public ViewResult Details(long id)
         {
-            Person person = context.People.Single(x => x.Id == id);
-            return View(person);
+            return View(_peopleRepository.Find(id));
         }
 
         //
         // GET: /People/Create
-
         public ActionResult Create()
         {
-            return View();
+            return View(new Person());
         } 
 
         //
         // POST: /People/Create
-
         [HttpPost]
         public ActionResult Create(Person person)
         {
             if (ModelState.IsValid)
             {
-                context.People.Add(person);
-                context.SaveChanges();
-                return RedirectToAction("Index");  
+                _peopleRepository.InsertOrUpdate(person);
+                _peopleRepository.Save();
+                return RedirectToAction("Index");
             }
-
-            return View(person);
+            else
+            {
+                return View(person);
+            }
         }
         
         //
         // GET: /People/Edit/5
- 
         public ActionResult Edit(long id)
         {
-            Person person = context.People.Single(x => x.Id == id);
-            return View(person);
+            return View(_peopleRepository.Find(id));
         }
 
         //
         // POST: /People/Edit/5
-
         [HttpPost]
         public ActionResult Edit(Person person)
         {
             if (ModelState.IsValid)
             {
-                context.Entry(person).State = EntityState.Modified;
-                context.SaveChanges();
+                _peopleRepository.InsertOrUpdate(person);
+                _peopleRepository.Save();
                 return RedirectToAction("Index");
             }
-            return View(person);
+            else
+            {
+                return View(person);
+            }
         }
 
         //
         // GET: /People/Delete/5
- 
         public ActionResult Delete(long id)
         {
-            Person person = context.People.Single(x => x.Id == id);
-            return View(person);
+            return View(_peopleRepository.Find(id));
         }
 
         //
         // POST: /People/Delete/5
-
         [HttpPost, ActionName("Delete")]
         public ActionResult DeleteConfirmed(long id)
         {
-            Person person = context.People.Single(x => x.Id == id);
-            context.People.Remove(person);
-            context.SaveChanges();
+            _peopleRepository.Delete(id);
+            _peopleRepository.Save();
+
             return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
         {
-            if (disposing) {
-                context.Dispose();
-            }
             base.Dispose(disposing);
         }
     }
